b7f27d9ce6fd4fe18c8ecbb1b68a4abb7ec8592e
dwalmsley23 <walmsley-d@ulster.ac.uk>
2026-05-02 17:34:54 +0100

    Added comments to all lines of function

--- NearestNeighbour.py
+++ NearestNeighbour.py
@@ -6,6 +6,7 @@ from parso.python.tree import ReturnStmt
 from shapely.geometry import Point
 from shapely.ops import nearest_points
 import math
+import folium
 
 
 #input the bus station data downloaded in a csv file
@@ -15,6 +16,15 @@ df = pd.read_csv('bus.csv') # read the csv data
 stations = gpd.GeoDataFrame(df[['CommonName']],
                             geometry=gpd.points_from_xy(df['Longitude'], df['Latitude']), # set the geometry using points_from_xy
                             crs='epsg:4326') # set the CRS using a text representation of the EPSG code for WGS84 lat/lon
+
+ni_boundary = gpd.read_file('OSNI_Open_Data_-_50K_Boundaries_-_NI_Outline.shp') #NI boundary shapefile
+ni_boundary = ni_boundary.to_crs(epsg=4326) #reprojected to lat/long
+
+stations = gpd.clip(stations, ni_boundary) #removal of stations not in NI
+
+m = stations.explore()
+m.save('map.html')
+
 #add new blank column to table called ID
 stations.insert(0,'ID', value=None)
 
@@ -22,42 +32,46 @@ stations.insert(0,'ID', value=None)
 for index,row in stations.iterrows():
     stations.loc[index,'ID'] = index
 
+
+
 #reproject the geodataframe to allow distance calculation- converted to Irish Tranverse Mercator
 stations = stations.to_crs(epsg=2157)
 
+
+
 #add new column to pgeodataframe into which will be placed the nearest neighbour point
 stations.insert(3, 'NearestNeighbour', None)
-
-#iterative process to find the nearest bus station for each of the bus stations in the geodataframe
-for index,row in stations.iterrows():
-    point = row.geometry #finds the location of each bus station
-    multipoint = stations.drop(index, axis=0).geometry.union_all() #creates a list of all other bus station locations except the bus station being considered
-    queried_geom, nearest_geo = nearest_points(point, multipoint) #uses nearest_points to find the closest bus station in list
-    stations.loc[index, 'NearestNeighbour'] = nearest_geo #adds the location of nearest bus tation to the nearest neighbour column
-
-
-#calculate the distance between each bus station and its nearest neighbour and add into Distance column
-for index,row in stations.iterrows():
-    row['Distance'] = row['geometry'].distance(row['NearestNeighbour'])/1000 #calculates distance in new column and divides by 1000 to get the distance in kilometres
-    stations.loc[index,'Distance'] = row['Distance']
-
-#calculate average distance between each bus station and nearest neighbour
-average_distance = (stations['Distance'].sum())/stations['Distance'].count()
-
-
-#calculate nearest neighbour value using NI area value
-NRvalue = 2*average_distance*math.sqrt(stations['ID'].count()/14330)
-
-#printing the value of NN
-print(f"The nearest neighbour value is {NRvalue:.3f}")
-
-#using value to assign a type of distribution
-if NRvalue < 1:
-    print("Bus stations in Northern Ireland have a  significantly clustered distribution")
-elif NRvalue == 1:
-    print("Bus stations in Northern Ireland have a significantly random distribution")
-else:
-    print("Bus stations in Northern Ireland have a significantly regular distribution")
+#
+# #iterative process to find the nearest bus station for each of the bus stations in the geodataframe
+# for index,row in stations.iterrows():
+#     point = row.geometry #finds the location of each bus station
+#     multipoint = stations.drop(index, axis=0).geometry.union_all() #creates a list of all other bus station locations except the bus station being considered
+#     queried_geom, nearest_geo = nearest_points(point, multipoint) #uses nearest_points to find the closest bus station in list
+#     stations.loc[index, 'NearestNeighbour'] = nearest_geo #adds the location of nearest bus tation to the nearest neighbour column
+#
+#
+# #calculate the distance between each bus station and its nearest neighbour and add into Distance column
+# for index,row in stations.iterrows():
+#     row['Distance'] = row['geometry'].distance(row['NearestNeighbour'])/1000 #calculates distance in new column and divides by 1000 to get the distance in kilometres
+#     stations.loc[index,'Distance'] = row['Distance']
+#
+# #calculate average distance between each bus station and nearest neighbour
+# average_distance = (stations['Distance'].sum())/stations['Distance'].count()
+#
+#
+# #calculate nearest neighbour value using NI area value
+# NRvalue = 2*average_distance*math.sqrt(stations['ID'].count()/14330)
+#
+# #printing the value of NN
+# print(f"The nearest neighbour value is {NRvalue:.3f}")
+#
+# #using value to assign a type of distribution
+# if NRvalue < 1:
+#     print("Bus stations in Northern Ireland have a  significantly clustered distribution")
+# elif NRvalue == 1:
+#     print("Bus stations in Northern Ireland have a significantly random distribution")
+# else:
+#     print("Bus stations in Northern Ireland have a significantly regular distribution")
 
 #input the post code data downloaded in a csv file
 dg = pd.read_csv('postcodes.csv')
@@ -112,10 +126,6 @@ while postcode != 'EXIT':
     print("--------------------------------------")
     print("Provide another postcode to find the closest bus station")
     postcode = str(input()).upper() #allows for multiple attempts at postcode bus station finder
-
-
-
-
 
 
 
